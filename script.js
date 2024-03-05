6055d69673cb52a1e0467f5990b52a678f120dcf
Patrick Gross <bears_lair@protonmail.com>
2024-03-05 15:16:12 -0800

    added border and submit button

---
+++ script.js
@@ -0,0 +1,8 @@
+const item = document.getElementById("item");
+const amount = document.getElementById("amount");
+const date = document.getElementById("date");
+const expenseTotal = document.getElementById("total");
+
+let expenseList = [];
+
+renderList() { };
