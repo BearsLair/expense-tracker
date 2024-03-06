576ca955de6b18d81eb1a91f87f55974d755ea10
Patrick Gross <bears_lair@protonmail.com>
2024-03-05 16:15:18 -0800

    expense constructor created

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const item = document.getElementById("item");
 const amount = document.getElementById("amount");
 const date = document.getElementById("date");
 const expenseTotal = document.getElementById("total");
+const submit = document.getElementById("total");
 
 let expenseList = [];
 
-renderList() { };
+function Expense(expenseId, expenseItem, expenseAmount, expenseDate) {
+  this.expenseId = expenseId;
+  this.expenseItem = expenseItem;
+  this.expenseAmount = expenseAmount;
+  this.expenseDate = expenseDate;
+}
+
+function addExpense() {}
+let expenseId = 0;
+
+if (expenseList.length === 0) {
+}
+// function renderList() { };
