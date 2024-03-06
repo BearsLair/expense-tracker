12101cfe53583c50d5ded6256ef8de0db2c51064
Patrick Gross <bears_lair@protonmail.com>
2024-03-05 16:43:54 -0800

    object creation functional

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const item = document.getElementById("item");
 const amount = document.getElementById("amount");
 const date = document.getElementById("date");
 const expenseTotal = document.getElementById("total");
-const submit = document.getElementById("total");
+const submitBtn = document.getElementById("submitBtn");
+const total = document.getElementById("total");
 
 let expenseList = [];
 
@@ -13,9 +14,35 @@ function Expense(expenseId, expenseItem, expenseAmount, expenseDate) {
   this.expenseDate = expenseDate;
 }
 
-function addExpense() {}
-let expenseId = 0;
+submitBtn.addEventListener("click", addExpense);
 
-if (expenseList.length === 0) {
+function addExpense() {
+  let expenseId = 0;
+
+  expenseList.length === 0
+    ? (expenseId = 1)
+    : (expenseId = expenseList.length + 1);
+
+  const expenseItem = item.value;
+  const expenseAmount = amount.value;
+  const expenseDate = date.value;
+
+  const addedExpense = new Expense(
+    expenseId,
+    expenseItem,
+    expenseAmount,
+    expenseDate
+  );
+
+  expenseList.push(addedExpense);
+
+  // TEST
+  console.log(expenseList);
+
+  // calculateTotal();
+  // renderList();
 }
+
+// function calculateTotal() {};
+
 // function renderList() { };
